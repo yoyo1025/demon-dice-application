62854d3dc071be32a443aff3c50fc833309f68a9
yoyo1025 <hsjejxys@gmail.com>
2025-01-25 22:44:51 +0900

    gorm導入&サンプルAPI作成

--- go.mod
+++ go.mod
@@ -5,14 +5,23 @@ go 1.22.7
 require github.com/labstack/echo/v4 v4.13.3
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.2 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.8.0 // indirect
+	gorm.io/driver/postgres v1.5.11 // indirect
+	gorm.io/gorm v1.25.12 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type User struct {
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"size:255"`
+	Age  int
+}
+
 func main() {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// DSN(Data Source Name)
+	dsn := fmt.Sprintf(
+    "host=%s port=%s user=%s password=%s dbname=%s",
+    dbHost, dbPort, dbUser, dbPassword, dbName,
+	)
+
+	// GORMでPostgreSQLに接続
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
+
+	// マイグレーション
+	db.AutoMigrate(&User{})
+
 	app := echo.New()
 
 	// BodyDumpミドルウェアを追加
@@ -17,11 +50,24 @@ func main() {
 		fmt.Printf("Response Body: %s\n", string(resBody))
 	}))
 
-	// ルートハンドラ
 	app.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello, world!!")
 	})
 
-	// サーバーをポート30000で起動
-	app.Logger.Fatal(app.Start(":30000"))
+	app.GET("/users", func(c echo.Context) error {
+		var users []User
+		db.Find(&users)
+		return c.JSON(http.StatusOK, users)
+	})
+
+	app.POST("/users", func(c echo.Context) error {
+		user := new(User)
+		if err := c.Bind(user); err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		db.Create(user)
+		return c.JSON(http.StatusOK, user)
+	})
+
+	app.Logger.Fatal(app.Start(":3000"))
 }

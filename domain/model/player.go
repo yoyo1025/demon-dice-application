366090cc0291bc409e5728036e07ddfd7d42032f
yoyo1025 <hsjejxys@gmail.com>
2025-02-09 17:36:38 +0900

    Playerのフィールドを公開範囲を変更

--- domain/model/player.go
+++ domain/model/player.go
@@ -1,19 +1,39 @@
 package model
 
 type Player struct {
-	ID          int64
-	UserID      int64
-	Name        string
-	IsConnected bool
-	IsOnBreak   bool
+	id          int64
+	userId      int64
+	name        string
+	isConnected bool
+	isOnBreak   bool
 }
 
 func NewPlayer(id, userId int64, name string, isConnected bool) *Player {
 	return &Player{
-		ID:          id,
-		UserID:      userId,
-		Name:        name,
-		IsConnected: isConnected,
-		IsOnBreak:   false,
+		id:          id,
+		userId:      userId,
+		name:        name,
+		isConnected: isConnected,
+		isOnBreak:   false,
 	}
+}
+
+func (p *Player) GetId() int64 {
+	return p.id;
+}
+
+func (p *Player) GetUserId() int64 {
+	return p.userId;
+}
+
+func (p *Player) GetName() string {
+	return p.name;
+}
+
+func (p *Player) GetIsConnected() bool {
+	return p.isConnected;
+}
+
+func (p *Player) GetIsOnBreak() bool {
+	return p.isOnBreak;
 }
--- domain/model/test/player_test.go
+++ domain/model/test/player_test.go
@@ -13,45 +13,19 @@ func TestNewPlayer(t *testing.T) {
 
 	player := model.NewPlayer(id, userId, name, isConnected)
 
-	if player.ID != id {
-		t.Errorf("expected ID to be %d, got %d", id, player.ID)
+	if player.GetId() != id {
+		t.Errorf("expected ID to be %d, got %d", id, player.GetId())
 	}
-	if player.UserID != userId {
-		t.Errorf("expected UserID to be %d, got %d", userId, player.UserID)
+	if player.GetUserId() != userId {
+		t.Errorf("expected UserID to be %d, got %d", userId, player.GetUserId())
 	}
-	if player.Name != name {
-		t.Errorf("expected Name to be %s, got %s", name, player.Name)
+	if player.GetName() != name {
+		t.Errorf("expected Name to be %s, got %s", name, player.GetName())
 	}
-	if player.IsConnected != isConnected {
-		t.Errorf("expected IsConnected to be %v, got %v", isConnected, player.IsConnected)
+	if player.GetIsConnected() != isConnected {
+		t.Errorf("expected IsConnected to be %v, got %v", isConnected, player.GetIsOnBreak())
 	}
-	if player.IsOnBreak != false {
-		t.Errorf("expected IsOnBreak to be false, got %v", player.IsOnBreak)
-	}
-}
-
-func TestPlayerSettersAndGetters(t *testing.T) {
-	player := model.NewPlayer(1, 1001, "InitialPlayer", true)
-
-	player.ID = 2
-	player.UserID = 2002
-	player.Name = "UpdatedPlayer"
-	player.IsConnected = false
-	player.IsOnBreak = true
-
-	if player.ID != 2 {
-		t.Errorf("expected ID to be %d, got %d", 2, player.ID)
-	}
-	if player.UserID != 2002 {
-		t.Errorf("expected UserID to be %d, got %d", 2002, player.UserID)
-	}
-	if player.Name != "UpdatedPlayer" {
-		t.Errorf("expected Name to be %s, got %s", "UpdatedPlayer", player.Name)
-	}
-	if player.IsConnected != false {
-		t.Errorf("expected IsConnected to be false, got %v", player.IsConnected)
-	}
-	if player.IsOnBreak != true {
-		t.Errorf("expected IsOnBreak to be true, got %v", player.IsOnBreak)
+	if player.GetIsOnBreak() != false {
+		t.Errorf("expected IsOnBreak to be false, got %v", player.GetIsConnected())
 	}
 }
